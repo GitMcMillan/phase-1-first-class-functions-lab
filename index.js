9a83f881174afcc0c055bec7d64a7fc958540f6f
alexandermcmillan <mcmillancontact@protonmail.com>
2023-10-15 19:12:02 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,33 +1,111 @@
 // Code your solution in this file!
 
-const returnFirstTwoDrivers = function (drivers) {
-    return drivers.slice(0, 2)
+
+let drivers = [`Antonia`, `Nuru`, `Amari`, `Mo`]
+
+
+const returnFirstTwoDrivers = function () {
+  return drivers.slice(0, 2);
+
 };
 
-const returnLastTwoDrivers = function (drivers) {
-    return drivers.slice(-2)
-}
+const returnLastTwoDrivers = function () {
+  return drivers.slice(-2)
+};
 
-
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
-
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(num) {
-    return function(number) {
-        return num * number
-    }
+  return function (number) {
+    return num * number
+  }
 
+};
+
+const fareDoubler = function (number) {
+  return number * 2
+};
+
+const fareTripler = function (number) {
+  return number * 3
+};
+
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+  return func(arrayOfDrivers)
 }
 
-function fareDoubler(num){
-    return num * 2
-  }
+// function selectDifferentDrivers(drivers, func) {
+//   return function func(returnFirstTwoDrivers) { }
 
-  function fareTripler(num) {
-    return num * 3
-  }
+// };
+
+//multiply the fare TIMES ITSELF, not by 4
+//one mistake was trying to push an anonymous function
+// function createFareMultiplier(num1) {
+//   return function () {
+//     num1 * num1
+//   }
+// };
 
 
-  function selectDifferentDrivers(arrayOfDrivers, func){
-    return func(arrayOfDrivers)
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const returnFirstTwoDrivers = function (drivers) {
+//     return drivers.slice(0, 2)
+// };
+
+// const returnLastTwoDrivers = function (drivers) {
+//     return drivers.slice(-2)
+// }
+
+
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+// function createFareMultiplier(num) {
+//     return function(number) {
+//         return num * number
+//     }
+
+// }
+
+// function fareDoubler(num){
+//     return num * 2
+//   }
+
+//   function fareTripler(num) {
+//     return num * 3
+//   }
+
+
+//   function selectDifferentDrivers(arrayOfDrivers, func){
+//     return func(arrayOfDrivers)
+//   }
